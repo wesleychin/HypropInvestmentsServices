8cb31decaa440dd23031599bc70b54ad6f047e3d
Wesley Chin <wesley@databuild.co.za>
2015-05-01 19:56:16 +0200

    Added firebase and can now insert data feed into firebase

--- test.js
+++ test.js
@@ -1,6 +1,9 @@
 var http = require('http');
 var parseString = require('xml2js').parseString;
+var Firebase = require('firebase');
+
 var xmlUrl = "http://feeds1.mcgbfa.com/engine.asmx/getPriceData?SubFeed=0&newfeed=true&CompanyKey=Hyprop%20Investments%20Ltd";
+var firebaseRootRef = new Firebase('https://boiling-heat-2151.firebaseio.com/shareinformation/datafeed');
 
 var req = http.get(xmlUrl, function(res) {
   var xml = '';
@@ -11,6 +14,7 @@ var req = http.get(xmlUrl, function(res) {
 
   res.on('end', function() {
     parseString(xml, function (err, result) {
+      firebaseRootRef.set(JSON.stringify(result));
       console.log(JSON.stringify(result));
     });
   });
